6fe7a89715f30a32e5b9625c7d54a14802b17272
Natalija Sasic <sasicnatalija4@gmail.com>
2021-12-10 07:05:16 +0100

    Add userRepository tests

--- springboot/src/test/java/com/backend/springboot/constants/UserConstans.java
+++ springboot/src/test/java/com/backend/springboot/constants/UserConstans.java
@@ -2,4 +2,9 @@ package com.backend.springboot.constants;
 
 public class UserConstans {
 	public static final String DB_USER_USERNAME = "masa";
+	public static final String DB_FIRED_USERNAME = "otpusteni";
+	public static final String DB_PASSWORD = "1234";
+	public static final String DB_USER_NAME = "NewName";
+	public static final String DB_USER_LASTNAME = "LastName";
+	public static final String DB_USER_NEW_USERNAME = "newUsername";
 }
