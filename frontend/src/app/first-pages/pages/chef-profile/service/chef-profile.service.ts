2335074f2f504531d260b422215a2aac377d4cbd
Tatjana Gavrilović <t.aca220799@hotmail.com>
2022-01-27 23:08:14 +0100

    new menu front done

--- frontend/src/app/first-pages/pages/chef-profile/service/chef-profile.service.ts
+++ frontend/src/app/first-pages/pages/chef-profile/service/chef-profile.service.ts
@@ -8,10 +8,16 @@ import { Main} from '../../../../../main';
 export class ChefProfileService {
 
   readonly URL : string = Main.PATH + "chef/get";
+  readonly ALL_MEALS_URL : string = Main.PATH + "meal/getAllMeals";
   
   constructor(private http: HttpClient) { }
 
   getChef(){
     return this.http.get(this.URL);
+  }
+
+
+  getAllMeals(){
+    return this.http.get(this.ALL_MEALS_URL);
   }
 }
--- springboot/src/main/java/com/backend/springboot/service/MenuService.java
+++ springboot/src/main/java/com/backend/springboot/service/MenuService.java
@@ -2,6 +2,7 @@ package com.backend.springboot.service;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.backend.springboot.dto.MealWithPriceDTO;
+import com.backend.springboot.model.Meal;
+import com.backend.springboot.model.MealPrice;
 import com.backend.springboot.model.Menu;
 import com.backend.springboot.model.MenuMealPrice;
 import com.backend.springboot.model.Restaurant;
+import com.backend.springboot.repository.MealPriceRepository;
+import com.backend.springboot.repository.MealRepository;
+import com.backend.springboot.repository.MenuMealPriceRepository;
 import com.backend.springboot.repository.MenuRepository;
 
 @Component
@@ -22,6 +29,14 @@ public class MenuService {
 	@Autowired
 	private MenuRepository menuRepository;
 	
+	@Autowired
+	private MealRepository mealRepository;
+	
+	@Autowired
+	private MealPriceRepository mealPriceRepository;
+	
+	@Autowired
+	private MenuMealPriceRepository menuMealPriceRepository;
 	
 	public Menu getCurrentMenu() {
 		Optional<Menu> current = menuRepository.findByCurrent();
@@ -42,28 +57,33 @@ public class MenuService {
         return null;
     }
 	
-	public boolean addNewMenu(Menu menu) {
+	
+	
+	public boolean addNewMenu(List<MealWithPriceDTO> list) {
+		Optional<Menu> old = menuRepository.findByCurrent();
+		old.get().setCurrent(false);
+		menuRepository.save(old.get());
+		
 		Restaurant restaurant = Restaurant.builder().id(1).build();
-		menu.setRestaurant(restaurant);
-		menuRepository.save(menu);
+		Menu newMenu = Menu.builder().restaurant(restaurant).current(true).dateOfValidation(LocalDateTime.now()).build();
+		menuRepository.save(newMenu);
+		
+		Optional<Menu> current = menuRepository.findByCurrent();
+		
+		for(MealWithPriceDTO m : list) {
+			Optional<Meal> meal = mealRepository.findById(m.getMealDTO().getId());
+			MealPrice mp = MealPrice.builder().meal(meal.get()).price(m.getPrice()).deleted(false).build();
+			mealPriceRepository.save(mp);
+			
+			List<MealPrice> all = mealPriceRepository.findAll();
+			MealPrice lastmp = all.get(all.size()-1);
+			
+			MenuMealPrice mmp = MenuMealPrice.builder().menu(current.get()).mealPrice(lastmp).deleted(false).build();
+			menuMealPriceRepository.save(mmp);
+		}
+		
 		return true;
 	}
 	
 	
-	@Scheduled(fixedDelayString = "PT24H")
-	public void SettingCurrentMenu() throws InterruptedException {
-		LocalDateTime now = LocalDateTime.now();
-		Optional<Menu> newMenu = menuRepository.findBydateTime(now);
-		
-		if(!newMenu.isEmpty()) {
-			Optional<Menu> current = menuRepository.findByCurrent();
-			current.get().setCurrent(false);
-			newMenu.get().setCurrent(true);
-			menuRepository.save(current.get());
-			menuRepository.save(newMenu.get());
-		}
-		
-		
-		
-	}
 }
